5505f9ef8c6b8a28dbc8f02ceecc1851e8641c2f
selmavangstein <vangsteins@carleton.edu>
2023-10-04 11:46:25 -0500

    more endpoints, some functions implemented

--- Game-Logic/gamelogic.py
+++ Game-Logic/gamelogic.py
@@ -1,3 +1,5 @@
+import random
+
 '''
 class Board
     - Which pieces where
@@ -29,31 +31,64 @@ class Board():
     def __init__(self, initialState):
         self.currentState = initialState
 
-    def move(player, move):
+    def move(self, player, move):
         #move piece
         #self.currentState = newState
         pass
 
 class Game():
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2): #id for human, maybe 0 for AI?
         self.player1 = player1
         self.player2 = player2
+    
+    def rollDice(self):
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        return (die1, die2)
+    
+    def getPossibleMoves(self, player, board): #should this player just be b(lack) or w(hite)?
+        #this is simple code that won't work, it just shows the idea!
+        possibleMoves = ()
+        i = 0
+        for entry in board:
+            if entry[0] == player:
+                possibleMoves.append(i, 2)
+            i+=1
+        return possibleMoves
+    
+    def getMove(self, player):
+        possibleMoves = self.getPossibleMoves(player, Board.board)
+        move = player.move(possibleMoves)
+        return
 
 
 class Player():
     '''send move to game class'''
     def __init__(self):
-        #get players from login/user interface?
-        #will there be a route for choosing players 
-        #(ie i want to play w a friend or w an ai)
-        #have to figure out how we know who is playing
+        #what needs to be common for the two?
+        #some move function, but they need to be implemented differently.
+        #should players keep track of their own pieces? Or should the game just send it a list of possible moves?
+        # # Or just the full game state for now?
         pass
 
 
 
 class HumanPlayer(Player):
+    #get players from login/user interface?
+    #will there be a route for choosing players 
+    #(ie i want to play w a friend or w an ai)
+    #have to figure out how we know who is playing
+        #some ID thing, but the backend game logic does not need to care, as long as we have it stored somewhere to send to the db
     def __init__(self):
             pass
 class AIPlayer(Player):
-    def __init__(self):
+    def __init__(self): #for the future: difficulty level?
         pass
+    def move(possibleMoves):
+        #remember that "first piece" is the opposite for the two players, since they play the board in opposite directions
+            #just think about how to handle that
+        #more complicated logic should come here
+        #find first piece that belongs to player
+        #tell to move it 2 places or so.
+        return possibleMoves[0]
+        
