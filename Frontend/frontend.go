a0f51edf51e24e57c157494e60783772b7039aab
Sophie Quinn <sophiequinn@Sophies-MacBook-Pro.local>
2023-10-10 23:12:38 -0500

    create and print initial array

---
+++ Frontend/frontend.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// initialState := map[string]string{"0": "", "1": "ww", "2": "", "3": "", "4": "", "5": "", "6": "bbbbbb", "7": "", "8": "bbb", "9": "", "10": "", "11": "", "12": "wwwwww",
+	// "13": "bbbbb", "14": "", "15": "", "16": "", "17": "www", "18": "", "19": "wwwww", "20": "", "21": "", "22": "", "23": "", "24": "bb", "25": ""}
+
+	initialState := [26]string {"", "ww", "", "", "", "", "bbbbb", "", "bbb", "", "", "", "wwwww", "bbbbb", "", "", "", "www", "", "wwwww", "", "", "", "", "bb", ""}
+	fmt.Println(initialState)
+
+	// for position, pieces := range initialState {
+	// 	fmt.Println(position)
+	// 	fmt.Println(pieces)
+	// }
+}
