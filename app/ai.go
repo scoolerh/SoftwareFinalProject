47529a041df4870588f03f807841f23de3ceda94
Selma Nybø Vangstein <104154926+selmavangstein@users.noreply.github.com>
2023-10-24 12:20:38 -0500

    Start Joe, bug fixes (#9)
    * planning for capturing pieces
    
    * capturing pieces
    
    * can only move captured pieces
    
    * draft of checkforwin and started restricting moving home
    
    * removed checkforwin, will be moved to other branch
    
    * merge conflict or smt
    
    * checking if moving pieces to home slot it allowed in getPossibleMoves
    
    * added comments, some refactoring
    
    * setting stuff up for testing
    
    * some more testing. updateState does not work.
    
    * fixed updateState and better testing
    
    * final bug fixes
    
    * small bug fix in bearing off for white
    
    * first draft updating rules for bearing off
    
    * debugging moves
    
    * Handling merge conflicts
    
    * double dice fix
    
    * removed redundant function
    
    * beginning pip count
    
    * dummy function for pips
    
    * designed package structure for AI
    
    * revert "designed package structure for AI"
    This reverts commit e70d554a059f0887b197f3034f5813deb8290776.
    
    * Joe considers pips and bug fixes

---
+++ app/ai.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func Testhandler() {
+	fmt.Println("success")
+}
+
+func Steve(possibleMoves []MoveType, color string) MoveType {
+	if color == "w" {
+		return possibleMoves[0]
+	} else {
+		return possibleMoves[len(possibleMoves)-1]
+	}
+}
+
+//need to be able to temporarily update state, then do countPips on that
+//so countPips should be separate from a game fcn
+
+func Joe(possibleMoves []MoveType, playerColor string, g Game) MoveType {
+	var opponentColor string
+	var bestMove MoveType
+	var bestScore int = 0
+	if playerColor == "w" {
+		opponentColor = "b"
+	} else {
+		opponentColor = "w"
+	}
+	for index, move := range possibleMoves {
+		_ = index
+		originalState := g.State
+		var score int
+		tempState := g.UpdateState(playerColor, move)
+		g.State = originalState //need to do this because updatestate changes the game. Make sure this does not cause any issues.
+		//Maybe we dont need it? We dont have *game - test this
+		pips := countPips(tempState, g.Captured)
+		score = pips[opponentColor] - pips[playerColor] //we want high opponentpip and low playerpip
+
+		//more checks to affect score here
+
+		if score > bestScore { //so we chose the first move to achieve the best score
+			bestScore = score
+			bestMove = move
+			log.Printf("bestMove updated")
+		}
+	}
+	return bestMove
+}
