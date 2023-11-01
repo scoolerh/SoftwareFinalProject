40f0dc1857c9e7f13592544a92bd9bfea8c54441
Han <hannahscooler@gmail.com>
2023-11-01 09:59:14 -0500

    working on pieces

---
+++ backgammon/app/static/pieces.js
@@ -0,0 +1,4 @@
+function setPieces() {
+    document.getElementById("white-piece").style.setProperty('--top-position', '0px');
+    document.getElementById("white-piece").style.setProperty('--left-position', '15px');
+}
