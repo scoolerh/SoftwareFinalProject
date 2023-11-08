ecc071380f2d814a35f458c0d1ea2f201e195da3
Sophie Quinn <sophiequinn@Sophies-MacBook-Pro.local>
2023-11-07 19:33:35 -0600

    fixing css

--- backgammon/app/api.go
+++ backgammon/app/api.go
@@ -314,7 +314,7 @@ func main() {
 	http.HandleFunc("/won", won)
 	http.HandleFunc("/db", dbHandler)
 	//http.HandleFunc("/scoreboard", scoreboard)
-	fs := http.FileServer(http.Dir("static"))
+	fs := http.FileServer(http.Dir("app/static"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":5555", nil) //listens for HTTP on port 9000, with standard mapping
 }
