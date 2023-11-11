f0afb2fde9ea9c9335fde93b5786a9319589f8c7
Barinamene Nwike <113485330+BNwike47@users.noreply.github.com>
2023-11-11 14:21:57 -0600

    Starting datavis development

--- db/db_api.py
+++ db/db_api.py
@@ -1,9 +1,19 @@
 import psycopg2
+import flask
 
 conn = psycopg2.connect(database="userstats",
                         user="statmaker",
+                        host = 'localhost'
+                        port = 5432
                         password="secure")
 
 cur = conn.cursor()
 
-cur.execute()
+cur.execute('SELECT * from userstats')
+table = cur.fetchall()
+username = table[0][0]
+print(username)
+
+username = 
+
+
